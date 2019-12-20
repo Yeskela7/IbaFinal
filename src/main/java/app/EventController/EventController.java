2eb96cfb4277166f87001f9fbdea25f6df0e6618
Isgkha <xalidisgenderov1@gmail.com>
2019-12-20 19:46:41 +0400

    from kema

---
+++ src/main/java/app/EventController/EventController.java
@@ -0,0 +1,37 @@
+package app.EventController;
+
+import app.entity.Event;
+import app.service.EventService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+@RestController
+@RequestMapping("/event")
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService service) {
+        this.eventService = service;
+    }
+
+    @GetMapping
+    public Iterable<Event> handle_get_all() {
+        return eventService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Event> handle_get(@PathVariable("id") long id) {
+        return eventService.getById(id);
+    }
+
+    @GetMapping("/{tag}")
+    public Iterable<Event> handle_get_tag(@PathVariable("tag") String tag) {
+        return eventService.getAllByTag(tag);
+    }
+
+    @GetMapping("/{title}")
+    public Optional<Event> handle_get_title(@PathVariable("title") String title) {
+        return eventService.getByTitle(title);
+    }
+}
