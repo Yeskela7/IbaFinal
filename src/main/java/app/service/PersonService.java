21b8f05883e84b2bb55e6880be4ad3738aa2899f
Mikayil <mikayil.yusubov@gmail.com>
2019-12-20 19:00:56 +0400

    PersonService

---
+++ src/main/java/app/service/PersonService.java
@@ -0,0 +1,11 @@
+package app.service;
+
+import app.entity.Person;
+
+import java.util.Optional;
+
+public interface PersonService {
+    public Optional<Person> getById(long id);
+    public Optional<Person> getByEmail(String email);
+    public Optional<Person> countPersonByEvents(long count);
+}
