a5eefafbc1494e6f862c8d9972c0169bc8922549
fidansadirli <mikayil.yusubov@gmail.com>
2019-12-21 13:07:43 +0400

    SecurityConfig

--- src/main/java/app/security/MyAppSecurityConfig.java
+++ src/main/java/app/security/MyAppSecurityConfig.java
@@ -1,4 +1,47 @@
 package app.security;
 
-public class MyAppSecurityConfig {
+import app.security.jwt.JwtAuthenticationFilter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Log4j2
+@Configuration
+@EnableWebSecurity
+
+public class MyAppSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtAuthenticationFilter jwtFilter;
+    public MyAppSecurityConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+
+        @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .authorizeRequests()
+                .antMatchers("/api/register/**", "/api/login/**").permitAll()
+                .antMatchers("/guest/**", "/api/guest/**").permitAll()
+                .antMatchers("/home/**", "/api/home/**").authenticated()
+                .antMatchers("/me/**", "/api/user/**").hasRole("USER")
+                .anyRequest().authenticated();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    @Bean
+    public AuthenticationManager myAuthenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
