9f6665f70ea0bc57569720d5ca0b9222c09fa076
Yeskela7 <alekseyh215@gmail.com>
2019-12-21 14:13:03 +0400

    --- filter

--- src/main/java/app/controller/PersonController.java
+++
@@ -1,4 +0,0 @@
-package app.controller;
-
-public class PersonController {
-}
--- src/main/java/app/mvc/MvcConfig.java
+++ src/main/java/app/mvc/MvcConfig.java
@@ -1,10 +1,13 @@
 package app.mvc;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig {
+@EnableWebMvc
+public class MvcConfig  implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         String[][] mappings = {
@@ -14,7 +17,6 @@ public class MvcConfig {
                 {"/home"  , "home" },
                 {"/admin" , "admin"},
                 {"/user"  , "user" },
-                {"/error"  , "error" },
         };
 
         for (String[] item: mappings) {
