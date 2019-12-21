c4282717b2631d35119694f4cde331a0fa977713
fidansadirli <mikayil.yusubov@gmail.com>
2019-12-21 13:37:24 +0400

    SecurityConfig

--- src/main/java/app/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/app/security/jwt/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import app.service.PersonServiceImpl;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
-
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final MyUserDetailsService uds;
