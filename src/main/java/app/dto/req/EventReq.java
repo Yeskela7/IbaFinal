f508e3bfe556daf0570b1b4aed81d1b35cd713d2
Aleksey Hajialiyev <Yeskela7@users.noreply.github.com>
2019-12-22 19:06:25 +0400

    --- controller

--- src/main/java/app/dto/req/EventReq.java
+++ src/main/java/app/dto/req/EventReq.java
@@ -15,36 +15,36 @@ public class EventReq {
     private Geo geo;
     private long time;
 
-//    public EventReq(String title, long creatorId, String description, String place, Geo geo, long time) {
-//        this.title = title;
-//        this.creatorId = creatorId;
-//        this.description = description;
-//        this.place = place;
-//        this.geo = geo;
-//        this.time = time;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public long getCreatorId() {
-//        return creatorId;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public Geo getGeo() {
-//        return geo;
-//    }
-//
-//    public String getPlace() {
-//        return place;
-//    }
-//
-//    public long getTime() {
-//        return time;
-//    }
+    public EventReq(String title, long creatorId, String description, String place, Geo geo, long time) {
+        this.title = title;
+        this.creatorId = creatorId;
+        this.description = description;
+        this.place = place;
+        this.geo = geo;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getCreatorId() {
+        return creatorId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public long getTime() {
+        return time;
+    }
 }
